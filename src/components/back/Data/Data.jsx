285f372ea568bf02398ec813beb58d3cb4bf528f
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-07-18 17:19:54 -0300

    marmitex

---
+++ src/components/back/Data/Data.jsx
@@ -0,0 +1,91 @@
+const data = {
+    productItems: [
+        {
+            id: "1",
+            name: "Carne bovina com beterraba",
+            price: 19.50,
+            image: "../public/pics/IMGMarmitex/MarmitexTradicional/Carne bovina com beterraba.jpeg"
+        },
+
+        {
+            id: "2",
+            name: "Carne bovina com purê",
+            price: 15.5,
+            image: "/public/pics/IMGMarmitex/MarmitexTradicional/Carne bovina com purê.jpeg"
+        },
+
+        {
+            id: "3",
+            name: "Carne bovina e frango empanado",
+            price: 22.5,
+            image: "/public/pics/IMGMarmitex/MarmitexTradicional/Carne bovina e frango empanado.jpeg"
+        },
+
+        {
+            id: "4",
+            name: "Cochas de frango com batatinha",
+            price: 17.5,
+            image:"/public/pics/IMGMarmitex/MarmitexTradicional/Cochas de frango com batatinha.jpeg"
+        },
+
+        {
+            id: "5",
+            name: "Tradicional/Frango empanado e couve",
+            price: 19.5,
+            image: "/public/pics/IMGMarmitex/MarmitexTradicional/Frango empanado e couve.jpeg"
+        },
+
+        {
+            id: "6",
+            name: "Marmita simples com farofa",
+            price: 12.5,
+            image: "/public/pics/IMGMarmitex/MarmitexTradicional/Marmita simples com farofa.jpeg"
+        },
+
+        {
+            id: "7",
+            name: "Abóbora, beterraba e alface",
+            price: 11.5,
+            image: "/public/pics/IMGMarmitex/MarmitexVegetariana/Abóbora, beterraba e alface.jpeg"
+        },
+
+        
+        {
+            id: "8",
+            name: "Arroz branco, feijão e brócolis",
+            price: 21.5,
+            image: "/public/pics/IMGMarmitex/MarmitexVegetariana/Arroz branco, feijão e brócolis.jpeg"
+        },
+        {
+            id: "9",
+            name: "Feijão macaça, azeitona e tomate",
+            price: 15.9,
+            image: "/public/pics/IMGMarmitex/MarmitexVegetariana/Feijão macaça, azeitona e tomate.jpeg"
+        },
+
+        {
+            id: "10",
+            name: "Feijão marrom, feijão macaça, arroz-e-soja",
+            price: 14.9,
+            image: "/public/pics/IMGMarmitex/MarmitexVegetariana/Feijão marrom, feijão macaça, arroz-e-soja.jpeg"
+        },
+
+        {
+            id: "11",
+            name: "Manga, brócolis e purê",
+            price: 12.9,
+            image: "/public/pics/IMGMarmitex/MarmitexVegetariana/Manga, brócolis e purê.jpeg"
+        },
+
+
+        {
+            id: "12",
+            name: "Porção de tomate, beterraba e alface",
+            price: 11.9,
+            image: "/public/pics/IMGMarmitex/MarmitexVegetariana/Porção de tomate, beterraba e alface.jpeg"
+        },
+    ]
+}
+
+
+export default data;
